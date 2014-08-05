e2686e9ec81c4691b8b52bc3a63c9839d3f2c6cb
Kristian Ruiz Kyvik <kristianruizkyvik@gmail.com>
2014-08-05 16:09:25 +0200

    button geolocation fixed

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,32 +1,39 @@
 var x = document.getElementById("getPosition");
 
-var get_location = function() {
- if (navigator.geolocation) {
-   navigator.geolocation.getCurrentPosition(showPosition,showError);
- } else {
-   x.innerHTML = "Geolocation is not supported by this browser.";
- }
-};
+function get_location() {
+   if (navigator.geolocation) {
+       navigator.geolocation.getCurrentPosition(showPosition,showError);
+   } else {
+       x.innerHTML = "Geolocation is not supported by this browser.";
+   }
+}
 
-var showPosition = function(position) {
- var latlon = position.coords.latitude+","+position.coords.longitude;
- var heading = 0;
- alert (latlon);
-};
+function showPosition(position) {
+   var latlon = position.coords.latitude+","+position.coords.longitude;
+   var heading = 0
+   // alert (latlon)
+   var url = "http://maps.googleapis.com/maps/api/staticmap?center=" + latlon + "&zoom=13&size=600x300&maptype=roadmap&markers=color:blue%7Clabel:S%7C" + latlon;
+   var image_url="<img src=" + url + ">";
+   document.getElementById("map").innerHTML =image_url;
 
-var showError = function(error) {
- switch(error.code) {
-   case error.PERMISSION_DENIED:
-   x.innerHTML = "User denied the request for Geolocation.";
-   break;
-   case error.POSITION_UNAVAILABLE:
-   x.innerHTML = "Location information is unavailable.";
-   break;
-   case error.TIMEOUT:
-   x.innerHTML = "The request to get user location timed out.";
-   break;
-   case error.UNKNOWN_ERROR:
-   x.innerHTML = "An unknown error occurred.";
-   break;
- }
-};
+
+
+
+}
+
+function showError(error) {
+   switch(error.code) {
+       case error.PERMISSION_DENIED:
+           x.innerHTML = "User denied the request for Geolocation."
+           break;
+       case error.POSITION_UNAVAILABLE:
+           x.innerHTML = "Location information is unavailable."
+           break;
+       case error.TIMEOUT:
+           x.innerHTML = "The request to get user location timed out."
+           break;
+       case error.UNKNOWN_ERROR:
+           x.innerHTML = "An unknown error occurred."
+           break;
+   }
+}
