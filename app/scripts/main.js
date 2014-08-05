29b3aa810e30f2af0d521b81c4541412324252b8
Kiko Beats <josefrancisco.verdu@gmail.com>
2014-08-05 15:38:29 +0200

    updated

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,32 +1,32 @@
 var x = document.getElementById("getPosition");
 
-function get_location() {
-   if (navigator.geolocation) {
-       navigator.geolocation.getCurrentPosition(showPosition,showError);
-   } else {
-       x.innerHTML = "Geolocation is not supported by this browser.";
-   }
-}
+var get_location = function() {
+ if (navigator.geolocation) {
+   navigator.geolocation.getCurrentPosition(showPosition,showError);
+ } else {
+   x.innerHTML = "Geolocation is not supported by this browser.";
+ }
+};
 
-function showPosition(position) {
-   var latlon = position.coords.latitude+","+position.coords.longitude;
-   var heading = 0
-   alert (latlon)
-}
+var showPosition = function(position) {
+ var latlon = position.coords.latitude+","+position.coords.longitude;
+ var heading = 0;
+ alert (latlon);
+};
 
-function showError(error) {
-   switch(error.code) {
-       case error.PERMISSION_DENIED:
-           x.innerHTML = "User denied the request for Geolocation."
-           break;
-       case error.POSITION_UNAVAILABLE:
-           x.innerHTML = "Location information is unavailable."
-           break;
-       case error.TIMEOUT:
-           x.innerHTML = "The request to get user location timed out."
-           break;
-       case error.UNKNOWN_ERROR:
-           x.innerHTML = "An unknown error occurred."
-           break;
-   }
-}
+var showError = function(error) {
+ switch(error.code) {
+   case error.PERMISSION_DENIED:
+   x.innerHTML = "User denied the request for Geolocation.";
+   break;
+   case error.POSITION_UNAVAILABLE:
+   x.innerHTML = "Location information is unavailable.";
+   break;
+   case error.TIMEOUT:
+   x.innerHTML = "The request to get user location timed out.";
+   break;
+   case error.UNKNOWN_ERROR:
+   x.innerHTML = "An unknown error occurred.";
+   break;
+ }
+};
